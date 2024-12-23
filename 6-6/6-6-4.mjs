7147ded1c475f2a416c473da460451767c0d9940
ten <nkhshc421038@g.neec.ac.jp>
2024-12-23 15:29:19 +0900

    6-6-4

---
+++ 6-6/6-6-4.mjs
@@ -0,0 +1,12 @@
+const map6_6_4 = new Map();
+
+map6_6_4.set("A", "カレー");
+map6_6_4.set("B", "定食");
+
+map6_6_4.delete("A");
+
+console.log(map6_6_4.get("A"));
+console.log(map6_6_4.get("B"));
+
+console.log(map6_6_4.has("A"));
+console.log(map6_6_4.has("B"));
