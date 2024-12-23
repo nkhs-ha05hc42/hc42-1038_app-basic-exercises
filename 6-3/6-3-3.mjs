6fc562f036b2e9e6c62d0e2d8a6d4a50c4754f98
ten <nkhshc421038@g.neec.ac.jp>
2024-12-23 13:50:22 +0900

    6-3-3

---
+++ 6-3/6-3-3.mjs
@@ -0,0 +1,7 @@
+const Array1 = [10, -5, 20, 30];
+const Array2 = Array1.some(a => a < 0);
+console.log(Array2);
+
+const array3 = [10, 20, 30, 40];
+const array4 = array3.some(a => a < 0);
+console.log(array4);
