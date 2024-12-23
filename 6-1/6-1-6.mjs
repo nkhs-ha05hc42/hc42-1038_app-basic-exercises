6ca5d1c6368fc89efc68dc647f521a2184f7d6a5
ten <nkhshc421038@g.neec.ac.jp>
2024-12-23 12:09:38 +0900

    6-1-6

---
+++ 6-1/6-1-6.mjs
@@ -0,0 +1,20 @@
+let a = 10;
+
+let b = 10;
+
+console.log(a > b);   
+console.log(a >= b); 
+console.log(a < b);   
+console.log(a <= b); 
+
+b = 9;
+console.log(a > b);    
+console.log(a >= b);  
+console.log(a < b);    
+console.log(a <= b);  
+
+b = 11;
+console.log(a > b);    
+console.log(a >= b); 
+console.log(a < b);    
+console.log(a <= b);  
