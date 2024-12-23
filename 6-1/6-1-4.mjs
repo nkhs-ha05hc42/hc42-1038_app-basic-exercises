e87b97c48fab6420e5b10a29396c3e609e65e63f
ten <nkhshc421038@g.neec.ac.jp>
2024-12-23 12:09:25 +0900

    6-1-4

---
+++ 6-1/6-1-4.mjs
@@ -0,0 +1,25 @@
+let a1 = false
+let b1 = false
+let a2 = true
+let b2 = true
+let a3 = true
+let b3 = false
+let a4 = true
+let b4 = false
+
+console.log(a1 && b1)
+console.log(a1 || b1)
+console.log(!a1)
+console.log(!b1)
+console.log(a2 && b2)
+console.log(a2 || b2)
+console.log(!a2)
+console.log(!b2)
+console.log(a3 && b3)
+console.log(a3 || b3)
+console.log(!a3)
+console.log(!b3)
+console.log(a4 && b4)
+console.log(a4 || b4)
+console.log(!a4)
+console.log(!b4)
