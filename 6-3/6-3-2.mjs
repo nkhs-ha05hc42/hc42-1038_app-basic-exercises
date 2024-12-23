82e84cb16e646a90ba4feb586c1e09c676d20db4
ten <nkhshc421038@g.neec.ac.jp>
2024-12-23 13:50:16 +0900

    6-3-2

---
+++ 6-3/6-3-2.mjs
@@ -0,0 +1,7 @@
+const Array1 = [51, 60, 100, 75];
+const Array2 = Array1.every(a => a > 50);
+console.log(Array2);
+
+const array3 = [51, 60, 40, 75];
+const array4 = array3.every(a => a > 50);
+console.log(array4);
