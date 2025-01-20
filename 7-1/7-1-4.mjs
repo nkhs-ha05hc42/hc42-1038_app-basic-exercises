44ab0bfb80ffe9f8c5e2e030773c06e696076975
ten <nkhshc421038@g.neec.ac.jp>
2025-01-20 11:01:53 +0900

    7-1-4

---
+++ 7-1/7-1-4.mjs
@@ -0,0 +1,12 @@
+const dat1 = {
+    name: "リンゴ",
+    value: "apple",
+  };
+  
+  const getValue = (ob) => {
+    return ob.value;
+  };
+  
+  const result = getValue(dat1);
+  console.log(result);
+  
