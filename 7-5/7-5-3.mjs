7b781c65409667969f1faf784f0b6b2397c3e958
ten <nkhshc421038@g.neec.ac.jp>
2025-01-20 14:45:14 +0900

    7-5-3

---
+++ 7-5/7-5-3.mjs
@@ -0,0 +1,34 @@
+const outputStar = () => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        console.log("★", new Date());
+        resolve();
+      }, 2000);
+    });
+  };
+  
+  const outputHeart = () => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        console.log("◆", new Date());
+        resolve();
+      }, 3000);
+    });
+  };
+  
+  const outputEnd = () => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        console.log("end", new Date());
+        resolve();
+      }, 5000);
+    });
+  };
+  
+  console.log("start", new Date());
+  
+  outputStar()
+    .then(() => {
+      return Promise.all([outputHeart(), outputEnd()]);
+    });
+  
