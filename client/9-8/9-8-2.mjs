235e6f2994072a1f7898a3d683b64f2fe01c557f
ten <nkhshc421038@g.neec.ac.jp>
2025-02-17 22:24:24 +0900

    9-8-2

--- client/9-8/9-8-2.mjs
+++ client/9-8/9-8-2.mjs
@@ -1,30 +1,41 @@
 const apiUrl = "http://localhost:3000/api/sample2";
 
-const tableBody = document.getElementById("data-table");
 
-const fetchDataAndDisplay = async () => {
-    try {
-        const response = await fetch(apiUrl);
-        const data = await response.json();
+window.onload = async () => {
+  const tableBody = document.getElementById("data-table-body");
 
-        data.forEach(person => {
-            const row = document.createElement("tr");
+    
+    const response = await fetch(apiUrl);
+    const data = await response.json();
 
-            row.innerHTML = `
-                <td>${person.id}</td>
-                <td>${person.name}</td>
-                <td>${person.age}</td>
-                <td>${person.postcode}</td>
-                <td>${person.address}</td>
-                <td>${person.averageScore}</td>
-            `;
+    
+    data.forEach(record => {
+      const tr = document.createElement("tr");
 
-            tableBody.appendChild(row);
-        });
-    } catch (error) {
-        console.error("データ取得エラー:", error);
-        tableBody.innerHTML = "<tr><td colspan='6'>データの取得に失敗しました</td></tr>";
-    }
-};
+      const idTd = document.createElement("td");
+      idTd.textContent = record.id;
+      tr.appendChild(idTd);
 
-fetchDataAndDisplay();
+      const nameTd = document.createElement("td");
+      nameTd.textContent = record.name;
+      tr.appendChild(nameTd);
+
+      const ageTd = document.createElement("td");
+      ageTd.textContent = record.age;
+      tr.appendChild(ageTd);
+
+      const postcodeTd = document.createElement("td");
+      postcodeTd.textContent = record.postcode;
+      tr.appendChild(postcodeTd);
+
+      const addressTd = document.createElement("td");
+      addressTd.textContent = record.address;
+      tr.appendChild(addressTd);
+
+      const scoreTd = document.createElement("td");
+      scoreTd.textContent = record.averageScore;
+      tr.appendChild(scoreTd);
+
+      tableBody.appendChild(tr);
+    });
+}
